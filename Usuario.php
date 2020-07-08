7e27ff374cd24ca6d2690ec116aa8bc881d9579e
Douglas Vallinhos] <douglas.m.vallinhos@gmail.com>
2020-07-08 00:42:09 -0300

    refatorado codigo pra separar logica de negocio

---
+++ Usuario.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Alura;
+
+class Usuario{
+    private $nome;
+    private $sobrenome;
+
+    public function __construct(string $nome){
+        $nomeSobrenome = explode(" ", $_POST['nome'], 2);
+
+        if($nomeSobrenome[0] === ""){
+            $this->nome = "Nome Inválido";
+        }else{
+            $this->nome = $nomeSobrenome[0];
+        }
+
+        if($nomeSobrenome[1] === null){
+            $this->sobrenome = "Sobrenome Inválido";
+        }else{
+            $this->sobrenome = $nomeSobrenome[1];
+        }
+
+    }
+
+
+    public function getNome():string{
+        return $this->nome;
+    }
+
+    public function getSobrenome():string{
+        return $this->sobrenome;
+    }
+
+
+
+
+}
