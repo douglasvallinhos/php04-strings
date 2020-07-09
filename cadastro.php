13a3e81e3ffc2277709192ce41c6fa9370f64a07
Douglas Vallinhos] <douglas.m.vallinhos@gmail.com>
2020-07-09 01:21:04 -0300

    adicionado o htmlspecialchars para proteger a impressao php do usuario digitar codigos nos campos

--- cadastro.php
+++ cadastro.php
@@ -19,13 +19,13 @@ $contato = new \Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['cep']
 <h1>Cadastro feito com sucesso.</h1>
 <p> <?= $usuario->getGenero() ?>, seguem os dados de sua conta:</p>
 <ul class="list-group">
-    <li class="list-group-item">Primeiro nome: <?= $usuario->getNome(); ?> </li>
-    <li class="list-group-item">Sobrenome: <?= $usuario->getSobrenome(); ?> </li>
-    <li class="list-group-item">Usuário: <?= $contato->getUsuario(); ?> </li>
-    <li class="list-group-item">Senha: <?= $usuario->getSenha(); ?> </li>
-    <li class="list-group-item">Telefone: <?= $contato->getTelefone(); ?> </li>
-    <li class="list-group-item">Email: <?= $contato->getEmail(); ?> </li>
-    <li class="list-group-item">Endereço: <?= $contato->getEndereco() . " - CEP: " . $contato->getCep(); ?> </li>
+    <li class="list-group-item">Primeiro nome: <?= htmlspecialchars($usuario->getNome()); ?> </li>
+    <li class="list-group-item">Sobrenome: <?= htmlspecialchars($usuario->getSobrenome()); ?> </li>
+    <li class="list-group-item">Usuário: <?= htmlspecialchars($contato->getUsuario()); ?> </li>
+    <li class="list-group-item">Senha: <?= htmlspecialchars($usuario->getSenha()); ?> </li>
+    <li class="list-group-item">Telefone: <?= htmlspecialchars($contato->getTelefone()); ?> </li>
+    <li class="list-group-item">Email: <?= htmlspecialchars($contato->getEmail()); ?> </li>
+    <li class="list-group-item">Endereço: <?= htmlspecialchars($contato->getEndereco() . " - CEP: " . $contato->getCep()); ?> </li>
 </ul>
 </div>
 </body>
